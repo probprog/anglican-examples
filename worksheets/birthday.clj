bf2564b9e689d79646f4fb93377de87aa856ccff
Frank Wood <fwood@robots.ox.ac.uk>
2015-04-07 17:00:57 +0100

    Updated and corrected birthday, complexityreduction, indian-gpa, and marsaglia.

--- worksheets/birthday.clj
+++ worksheets/birthday.clj
@@ -33,13 +33,21 @@
     (if (not (empty? (filter #(= (first x) %) (rest x))))
       true
       (equal-pair-in-sequence (rest x)))))
+
+
+(defn duplicates? [xs]
+  (loop [[x & xs :as coll] xs, seen #{}]
+    (if (empty? coll) false
+      (or (contains? seen x) (recur xs (conj seen x))))))
+
+
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/equal-pair-in-sequence</span>","value":"#'pdia/equal-pair-in-sequence"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/duplicates?</span>","value":"#'pdia/duplicates?"}
 ;; <=
 
 ;; @@
-(with-primitive-procedures [equal-pair-in-sequence]
+(with-primitive-procedures [equal-pair-in-sequence duplicates?]
   (defquery equal-birthday-in-a-group-of N 
     "birthday: takes number of people in room"
      (let [birthday-distribution (uniform-discrete 1 366)
@@ -68,7 +76,7 @@
 (float (/ (count (filter true? samples)) (count samples)))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-unkown'>0.523</span>","value":"0.523"}
+;;; {"type":"html","content":"<span class='clj-unkown'>0.488</span>","value":"0.488"}
 ;; <=
 
 ;; **
@@ -102,13 +110,6 @@
 ;; @@
 (time (plot/histogram (take 5000 u)))
 ;; @@
-;; ->
-;;; &quot;Elapsed time: 1213887.240166 msecs&quot;
-;;; 
-;; <-
-;; =>
-;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"e6395e57-91c9-4b6f-8915-fb19bdb5921f","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"e6395e57-91c9-4b6f-8915-fb19bdb5921f","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"e6395e57-91c9-4b6f-8915-fb19bdb5921f"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"e6395e57-91c9-4b6f-8915-fb19bdb5921f","values":[{"x":38.0,"y":0},{"x":39.0,"y":5000.0},{"x":40.0,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"e6395e57-91c9-4b6f-8915-fb19bdb5921f\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"e6395e57-91c9-4b6f-8915-fb19bdb5921f\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"e6395e57-91c9-4b6f-8915-fb19bdb5921f\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"e6395e57-91c9-4b6f-8915-fb19bdb5921f\", :values ({:x 38.0, :y 0} {:x 39.0, :y 5000.0} {:x 40.0, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
-;; <=
 
 ;; **
 ;;; Visually it would appear that this occurs around 38-40 people.  We can algorithmically find the mode for potential subsequent use:
@@ -124,9 +125,6 @@
      (* bucket-size))
 
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-long'>38</span>","value":"38"}
-;; <=
 
 ;; @@
 
--- worksheets/marsaglia.clj
+++ worksheets/marsaglia.clj
@@ -49,7 +49,7 @@
 
 ;; @@
 ;; =>
-;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"ca5713b9-7f20-44a2-a0f9-c477e9de5752","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"ca5713b9-7f20-44a2-a0f9-c477e9de5752","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"ca5713b9-7f20-44a2-a0f9-c477e9de5752"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"ca5713b9-7f20-44a2-a0f9-c477e9de5752","values":[{"x":-5.783807445696503,"y":0},{"x":-4.781623183541953,"y":7.0E-4},{"x":-3.7794389213874022,"y":0.0024},{"x":-2.7772546592328515,"y":0.008},{"x":-1.7750703970783008,"y":0.0213},{"x":-0.7728861349237501,"y":0.0505},{"x":0.22929812723080056,"y":0.1037},{"x":1.2314823893853513,"y":0.1583},{"x":2.233666651539902,"y":0.1972},{"x":3.2358509136944527,"y":0.1882},{"x":4.238035175849003,"y":0.1361},{"x":5.240219438003553,"y":0.0763},{"x":6.242403700158103,"y":0.0395},{"x":7.244587962312654,"y":0.0131},{"x":8.246772224467204,"y":0.0038},{"x":9.248956486621754,"y":8.0E-4},{"x":10.251140748776304,"y":1.0E-4},{"x":11.253325010930855,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"ca5713b9-7f20-44a2-a0f9-c477e9de5752\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"ca5713b9-7f20-44a2-a0f9-c477e9de5752\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"ca5713b9-7f20-44a2-a0f9-c477e9de5752\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"ca5713b9-7f20-44a2-a0f9-c477e9de5752\", :values ({:x -5.783807445696503, :y 0} {:x -4.781623183541953, :y 7.0E-4} {:x -3.7794389213874022, :y 0.0024} {:x -2.7772546592328515, :y 0.008} {:x -1.7750703970783008, :y 0.0213} {:x -0.7728861349237501, :y 0.0505} {:x 0.22929812723080056, :y 0.1037} {:x 1.2314823893853513, :y 0.1583} {:x 2.233666651539902, :y 0.1972} {:x 3.2358509136944527, :y 0.1882} {:x 4.238035175849003, :y 0.1361} {:x 5.240219438003553, :y 0.0763} {:x 6.242403700158103, :y 0.0395} {:x 7.244587962312654, :y 0.0131} {:x 8.246772224467204, :y 0.0038} {:x 9.248956486621754, :y 8.0E-4} {:x 10.251140748776304, :y 1.0E-4} {:x 11.253325010930855, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"4513c37f-070e-43cf-bdc9-dafee20f591b","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"4513c37f-070e-43cf-bdc9-dafee20f591b","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"4513c37f-070e-43cf-bdc9-dafee20f591b"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"4513c37f-070e-43cf-bdc9-dafee20f591b","values":[{"x":-5.0939393840176255,"y":0},{"x":-4.120815714113567,"y":0.0019},{"x":-3.147692044209508,"y":0.0045},{"x":-2.1745683743054496,"y":0.0136},{"x":-1.201444704401391,"y":0.0361},{"x":-0.22832103449733232,"y":0.082},{"x":0.7448026354067263,"y":0.1293},{"x":1.717926305310785,"y":0.1798},{"x":2.6910499752148436,"y":0.1922},{"x":3.664173645118902,"y":0.1589},{"x":4.637297315022961,"y":0.1057},{"x":5.6104209849270195,"y":0.0568},{"x":6.583544654831078,"y":0.026},{"x":7.556668324735137,"y":0.0101},{"x":8.529791994639195,"y":0.0023},{"x":9.502915664543254,"y":8.0E-4},{"x":10.476039334447313,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"4513c37f-070e-43cf-bdc9-dafee20f591b\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"4513c37f-070e-43cf-bdc9-dafee20f591b\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"4513c37f-070e-43cf-bdc9-dafee20f591b\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"4513c37f-070e-43cf-bdc9-dafee20f591b\", :values ({:x -5.0939393840176255, :y 0} {:x -4.120815714113567, :y 0.0019} {:x -3.147692044209508, :y 0.0045} {:x -2.1745683743054496, :y 0.0136} {:x -1.201444704401391, :y 0.0361} {:x -0.22832103449733232, :y 0.082} {:x 0.7448026354067263, :y 0.1293} {:x 1.717926305310785, :y 0.1798} {:x 2.6910499752148436, :y 0.1922} {:x 3.664173645118902, :y 0.1589} {:x 4.637297315022961, :y 0.1057} {:x 5.6104209849270195, :y 0.0568} {:x 6.583544654831078, :y 0.026} {:x 7.556668324735137, :y 0.0101} {:x 8.529791994639195, :y 0.0023} {:x 9.502915664543254, :y 8.0E-4} {:x 10.476039334447313, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
 ;; <=
 
 ;; **
@@ -66,7 +66,7 @@
     (observe [this value] (observe (normal m (sqrt v)) value)))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-unkown'>#&lt;MultiFn clojure.lang.MultiFn@714df2c8&gt;</span>","value":"#<MultiFn clojure.lang.MultiFn@714df2c8>"}
+;;; {"type":"html","content":"<span class='clj-unkown'>#&lt;MultiFn clojure.lang.MultiFn@5110ec0c&gt;</span>","value":"#<MultiFn clojure.lang.MultiFn@5110ec0c>"}
 ;; <=
 
 ;; **
@@ -85,7 +85,11 @@
             (+ mean (* (sqrt var)
 				       (* x (sqrt (* -2 (/ ( log s) s ))))))
             (marsaglia-normal mean var))))
+;(marsaglia-normal 8 9 (prn) {})
 ;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;marsaglia/marsaglia-normal</span>","value":"#'marsaglia/marsaglia-normal"}
+;; <=
 
 ;; **
 ;;; What is interesting about this is that it exposes the internal random state of the `marsaglia-normal` to the inference engine.  This may or may not be a good idea depending on a number of issues.
@@ -94,20 +98,39 @@
 ;; **
 
 ;; @@
-
-  (defquery unknown-mean [] 
+(defquery unknown-mean-1 [d] 
     (let [sigma (sqrt 2)
           mu (marsaglia-normal 1 5)]
+      
           (observe (normal mu sigma) 9)
           (observe (normal mu sigma) 8)
+
           (predict :mu mu)))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;marsaglia/unknown-mean</span>","value":"#'marsaglia/unknown-mean"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;marsaglia/unknown-mean-1</span>","value":"#'marsaglia/unknown-mean-1"}
 ;; <=
 
 ;; **
-;;; And here are the results.  One can use any of the available inference engines to draw samples from the conditional distribution defined by this query.
+;;; And here is the same in a more idiomatically Clojure-esq way.
+;; **
+
+;; @@
+  (def data [9 8])
+
+  (defquery unknown-mean-2 [d] 
+    (let [sigma (sqrt 2)
+          mu (marsaglia-normal 1 5)
+          obsdist (normal mu sigma)]
+          (map (fn [d] (observe obsdist d)) data)
+          (predict :mu mu)))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;marsaglia/unknown-mean-2</span>","value":"#'marsaglia/unknown-mean-2"}
+;; <=
+
+;; **
+;;; |And here are the results.  One can use any of the available inference engines to draw samples from the conditional distribution defined by this query.
 ;; **
 
 ;; **
@@ -120,13 +143,13 @@
 [:lmh :pgibbs :pcascade :smc]
 
 
-(->> (doquery :lmh unknown-mean nil :number-of-particles 1000)
+(->> (doquery :lmh unknown-mean-1 nil :number-of-particles 1000)
      (map get-predicts)
      (map :mu)
      (take 10000)
      (#(plot/histogram % :normalize :probability)))
 
-(->> (doquery :lmh unknown-mean nil :number-of-particles 1000)
+(->> (doquery :lmh unknown-mean-1 nil :number-of-particles 1000)
      (map get-predicts)
      (map :mu)
      (take 10000)
@@ -135,9 +158,9 @@
   
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-double'>7.205971502344663</span>","value":"7.205971502344663"}
+;;; {"type":"html","content":"<span class='clj-double'>7.210233842940729</span>","value":"7.210233842940729"}
 ;; <=
 
-;; @@
-
-;; @@
+;; **
+;;; Which is near the analytical posterior.
+;; **
