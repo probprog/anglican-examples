3e7179b81c67b70e2c41263357824497f001d9ac
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2015-12-02 15:34:51 +0000

    updated crp aistats to show bar plots, add fixme statement
    something is wrong with the parametrization in this example.
    will fix this on next maintenance pass.

--- worksheets/crp-aistats.clj
+++ worksheets/crp-aistats.clj
@@ -38,7 +38,7 @@
 ;; <=
 
 ;; **
-;;; Define model
+;;; ## Define model
 ;; **
 
 ;; @@
@@ -72,7 +72,7 @@
 ;; <=
 
 ;; **
-;;; Define data and posterior
+;;; ## Define data and true posterior
 ;; **
 
 ;; @@
@@ -93,31 +93,68 @@
 ;; <=
 
 ;; **
-;;; Run inference
+;;; ## Run inference
 ;; **
 
 ;; @@
-(def number-of-particles 1000)
-(def number-of-samples 100000)
+(def number-of-particles 10)
+(def number-of-samples 1000)
 
 (def samples
-  (->> (doquery :smc crp-mixture
+  (->> (doquery :pgas crp-mixture
                 [data 1.72 0.0 100.0 1.0 10.0]
                 :number-of-particles number-of-particles)
        (take number-of-samples)
        doall
        time))
 ;; @@
-;; ->
-;;; &quot;Elapsed time: 55802.416 msecs&quot;
-;;; 
-;; <-
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;aistats-examples/samples</span>","value":"#'aistats-examples/samples"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;aistats-examples/number-of-samples</span>","value":"#'aistats-examples/number-of-samples"}
 ;; <=
 
 ;; **
-;;; Calculate L2 error relative to true posterior as a function of number of samples
+;;; ## Plot Empirical and True Posterior
+;; **
+
+;; @@
+(def empirical-posterior 
+  (->> samples
+       (collect-by :num-clusters)
+       s/empirical-distribution
+  	   (into (sorted-map))))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;aistats-examples/empirical-posterior</span>","value":"#'aistats-examples/empirical-posterior"}
+;; <=
+
+;; **
+;;; **Empirical Posterior**
+;; **
+
+;; @@
+(plot/bar-chart (sort (keys posterior))
+                (map #(get empirical-posterior % 0.0) 
+                     (sort (keys posterior)))) 
+;; @@
+;; =>
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"7bfef1d6-5aa0-4ac3-9009-f9fd79006680","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"7bfef1d6-5aa0-4ac3-9009-f9fd79006680","field":"data.y"}}],"marks":[{"type":"rect","from":{"data":"7bfef1d6-5aa0-4ac3-9009-f9fd79006680"},"properties":{"enter":{"y":{"scale":"y","field":"data.y"},"width":{"offset":-1,"scale":"x","band":true},"x":{"scale":"x","field":"data.x"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"data":[{"name":"7bfef1d6-5aa0-4ac3-9009-f9fd79006680","values":[{"x":1,"y":0.0},{"x":2,"y":0.06840000000000114},{"x":3,"y":0.48046999999999956},{"x":4,"y":0.3556199999999975},{"x":5,"y":0.08710000000000163},{"x":6,"y":0.00802000000000012},{"x":7,"y":3.900000000000061E-4},{"x":8,"y":0.0},{"x":9,"y":0.0},{"x":10,"y":0.0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"7bfef1d6-5aa0-4ac3-9009-f9fd79006680\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"7bfef1d6-5aa0-4ac3-9009-f9fd79006680\", :field \"data.y\"}}], :marks [{:type \"rect\", :from {:data \"7bfef1d6-5aa0-4ac3-9009-f9fd79006680\"}, :properties {:enter {:y {:scale \"y\", :field \"data.y\"}, :width {:offset -1, :scale \"x\", :band true}, :x {:scale \"x\", :field \"data.x\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :data [{:name \"7bfef1d6-5aa0-4ac3-9009-f9fd79006680\", :values ({:x 1, :y 0.0} {:x 2, :y 0.06840000000000114} {:x 3, :y 0.48046999999999956} {:x 4, :y 0.3556199999999975} {:x 5, :y 0.08710000000000163} {:x 6, :y 0.00802000000000012} {:x 7, :y 3.900000000000061E-4} {:x 8, :y 0.0} {:x 9, :y 0.0} {:x 10, :y 0.0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;; <=
+
+;; **
+;;; **True Posterior**
+;; **
+
+;; @@
+(let [p (into (sorted-map) posterior)]
+	(plot/bar-chart (keys p)
+    	            (vals p)))
+;; @@
+;; =>
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"70154361-57e0-48be-aa5c-6f3acba8d28b","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"70154361-57e0-48be-aa5c-6f3acba8d28b","field":"data.y"}}],"marks":[{"type":"rect","from":{"data":"70154361-57e0-48be-aa5c-6f3acba8d28b"},"properties":{"enter":{"y":{"scale":"y","field":"data.y"},"width":{"offset":-1,"scale":"x","band":true},"x":{"scale":"x","field":"data.x"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"data":[{"name":"70154361-57e0-48be-aa5c-6f3acba8d28b","values":[{"x":1,"y":1.0458453073364188E-5},{"x":2,"y":0.3521493160516654},{"x":3,"y":0.40147902040466416},{"x":4,"y":0.19103473668703566},{"x":5,"y":0.04808427876972766},{"x":6,"y":0.006748061503565986},{"x":7,"y":5.090827403037801E-4},{"x":8,"y":1.7630150940851895E-5},{"x":9,"y":1.6031904113694912E-7},{"x":10,"y":3.9501396390265077E-10}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"70154361-57e0-48be-aa5c-6f3acba8d28b\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"70154361-57e0-48be-aa5c-6f3acba8d28b\", :field \"data.y\"}}], :marks [{:type \"rect\", :from {:data \"70154361-57e0-48be-aa5c-6f3acba8d28b\"}, :properties {:enter {:y {:scale \"y\", :field \"data.y\"}, :width {:offset -1, :scale \"x\", :band true}, :x {:scale \"x\", :field \"data.x\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :data [{:name \"70154361-57e0-48be-aa5c-6f3acba8d28b\", :values ({:x 1, :y 1.0458453073364188E-5} {:x 2, :y 0.3521493160516654} {:x 3, :y 0.40147902040466416} {:x 4, :y 0.19103473668703566} {:x 5, :y 0.04808427876972766} {:x 6, :y 0.006748061503565986} {:x 7, :y 5.090827403037801E-4} {:x 8, :y 1.7630150940851895E-5} {:x 9, :y 1.6031904113694912E-7} {:x 10, :y 3.9501396390265077E-10})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
+;; <=
+
+;; **
+;;; ## Plot L2 error relative to true posterior as a function of number of samples
 ;; **
 
 ;; @@
@@ -150,5 +187,9 @@
                 :y-title "log L2 error")
 ;; @@
 ;; =>
-;;; {"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":50,"bottom":20,"right":10},"data":[{"name":"41480058-43dc-42a8-9204-d4ab65b1d5ad","values":[{"x":2.9999999999999996,"y":-0.9669234864200613},{"x":3.301029995663981,"y":-0.16131507351386684},{"x":3.6989700043360187,"y":-0.28906146659682513},{"x":4.0,"y":-0.2133681664221509},{"x":4.30102999566398,"y":-0.2506125509669224},{"x":4.698970004336019,"y":-0.35667809223826724},{"x":5.0,"y":-0.3926010749243385}]}],"marks":[{"type":"line","from":{"data":"41480058-43dc-42a8-9204-d4ab65b1d5ad"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"stroke":{"value":"#05A"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"41480058-43dc-42a8-9204-d4ab65b1d5ad","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"41480058-43dc-42a8-9204-d4ab65b1d5ad","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 50, :bottom 20, :right 10}, :data [{:name \"41480058-43dc-42a8-9204-d4ab65b1d5ad\", :values ({:x 2.9999999999999996, :y -0.9669234864200613} {:x 3.301029995663981, :y -0.16131507351386684} {:x 3.6989700043360187, :y -0.28906146659682513} {:x 4.0, :y -0.2133681664221509} {:x 4.30102999566398, :y -0.2506125509669224} {:x 4.698970004336019, :y -0.35667809223826724} {:x 5.0, :y -0.3926010749243385})}], :marks [{:type \"line\", :from {:data \"41480058-43dc-42a8-9204-d4ab65b1d5ad\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#05A\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"41480058-43dc-42a8-9204-d4ab65b1d5ad\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"41480058-43dc-42a8-9204-d4ab65b1d5ad\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}
+;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"62afaea7-9f36-4bd4-ab48-53ea4aebd886","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"62afaea7-9f36-4bd4-ab48-53ea4aebd886","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"62afaea7-9f36-4bd4-ab48-53ea4aebd886"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"stroke":{"value":"#05A"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"62afaea7-9f36-4bd4-ab48-53ea4aebd886","values":[{"x":2.9999999999999996,"y":-0.44539414061794586},{"x":3.301029995663981,"y":-0.4274413187644241},{"x":3.6989700043360187,"y":-1.341538635489278},{"x":4.0,"y":-0.8480653759107515},{"x":4.30102999566398,"y":-0.7503771128544283},{"x":4.698970004336019,"y":-0.6104560587323127},{"x":5.0,"y":-0.6051002143743783}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"62afaea7-9f36-4bd4-ab48-53ea4aebd886\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"62afaea7-9f36-4bd4-ab48-53ea4aebd886\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"62afaea7-9f36-4bd4-ab48-53ea4aebd886\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#05A\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"62afaea7-9f36-4bd4-ab48-53ea4aebd886\", :values ({:x 2.9999999999999996, :y -0.44539414061794586} {:x 3.301029995663981, :y -0.4274413187644241} {:x 3.6989700043360187, :y -1.341538635489278} {:x 4.0, :y -0.8480653759107515} {:x 4.30102999566398, :y -0.7503771128544283} {:x 4.698970004336019, :y -0.6104560587323127} {:x 5.0, :y -0.6051002143743783})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
 ;; <=
+
+;; @@
+
+;; @@
