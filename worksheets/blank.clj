d8eacad83c29a86a2a7a9f69445399b32618e177
Jan-Willem van de Meent <jwvdm@robots.ox.ac.uk>
2016-03-21 16:24:35 +0000

    added blank worksheet (only contains namespace declaration)

---
+++ worksheets/blank.clj
@@ -0,0 +1,30 @@
+;; gorilla-repl.fileformat = 1
+
+;; **
+;;; # Blank Worksheet 
+;; **
+
+;; **
+;;; (required namespace declarations only)
+;; **
+
+;; @@
+(ns blank
+  (:require [gorilla-plot.core :as plot]
+            [clojure.java.io :as io]
+            [clojure.data.json :as json]
+            [clojure.core.matrix :as mat]
+            [anglican.core 
+             :refer [doquery]]
+            [anglican.emit 
+             :refer [query defquery conditional fm defm]]
+           	[anglican.inference 
+             :refer [equalize collect-by log-marginal]]
+            [anglican.stat
+             :refer [empirical-distribution empirical-expectation 
+                     empirical-mean empirical-variance empirical-std empirical-skew empirical-kurtosis
+                     sum mean variance std skew kurtosis l2-norm]]))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
+;; <=
