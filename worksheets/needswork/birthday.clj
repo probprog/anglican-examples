7c404afadc0887880d3a23adda8371b42070dd20
Brooks <brooks@robots.ox.ac.uk>
2016-03-21 20:57:49 +0000

    The "birthday" worksheet should be modified to use `conditional`, or something -- at the moment, it is not the most readable example of nested inference...

--- worksheets/needswork/birthday.clj
+++ worksheets/needswork/birthday.clj
@@ -9,12 +9,10 @@
 ;; **
 
 ;; @@
-(ns pdia
+(ns birthday
   (:require [gorilla-plot.core :as plot])
   (:use clojure.repl
-        [anglican core runtime emit [state :only [get-predicts]]]
-        [anglib crp]
-        [clojure.string :only (join split blank?)]))
+        [anglican core runtime emit [state :only [get-predicts]]]))
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
@@ -42,7 +40,7 @@
 
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/duplicates?</span>","value":"#'pdia/duplicates?"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;birthday/duplicates?</span>","value":"#'birthday/duplicates?"}
 ;; <=
 
 ;; @@
@@ -57,7 +55,7 @@
           
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/equal-birthday-in-a-group-of</span>","value":"#'pdia/equal-birthday-in-a-group-of"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;birthday/equal-birthday-in-a-group-of</span>","value":"#'birthday/equal-birthday-in-a-group-of"}
 ;; <=
 
 ;; @@
@@ -66,7 +64,7 @@
 (def t (map #(if % 1 0) (map :eq (map get-predicts sampler))))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/t</span>","value":"#'pdia/t"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;birthday/t</span>","value":"#'birthday/t"}
 ;; <=
 
 ;; @@
@@ -75,7 +73,7 @@
 (float (/ (count (filter true? samples)) (count samples)))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-unkown'>0.497</span>","value":"0.497"}
+;;; {"type":"html","content":"<span class='clj-unkown'>0.5</span>","value":"0.5"}
 ;; <=
 
 ;; **
@@ -94,7 +92,7 @@
        (predict :N num-people-in-room))))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/num-people-in-room-needed-to-have-X-percent-chance-of-overlapping-birthday</span>","value":"#'pdia/num-people-in-room-needed-to-have-X-percent-chance-of-overlapping-birthday"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;birthday/num-people-in-room-needed-to-have-X-percent-chance-of-overlapping-birthday</span>","value":"#'birthday/num-people-in-room-needed-to-have-X-percent-chance-of-overlapping-birthday"}
 ;; <=
 
 ;; @@
@@ -103,19 +101,12 @@
 
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;pdia/u</span>","value":"#'pdia/u"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;birthday/u</span>","value":"#'birthday/u"}
 ;; <=
 
 ;; @@
 (time (plot/histogram (take 1000 u)))
 ;; @@
-;; ->
-;;; &quot;Elapsed time: 24749.52992 msecs&quot;
-;;; 
-;; <-
-;; =>
-;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"fc5bfc6c-c205-4558-b66b-f847062b8cf8","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"fc5bfc6c-c205-4558-b66b-f847062b8cf8","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"fc5bfc6c-c205-4558-b66b-f847062b8cf8"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"fc5bfc6c-c205-4558-b66b-f847062b8cf8","values":[{"x":23.0,"y":0},{"x":25.363636363636363,"y":140.0},{"x":27.727272727272727,"y":30.0},{"x":30.09090909090909,"y":100.0},{"x":32.45454545454545,"y":30.0},{"x":34.81818181818182,"y":90.0},{"x":37.18181818181819,"y":60.0},{"x":39.545454545454554,"y":90.0},{"x":41.90909090909092,"y":20.0},{"x":44.27272727272729,"y":200.0},{"x":46.636363636363654,"y":110.0},{"x":49.00000000000002,"y":130.0},{"x":51.36363636363639,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"fc5bfc6c-c205-4558-b66b-f847062b8cf8\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"fc5bfc6c-c205-4558-b66b-f847062b8cf8\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"fc5bfc6c-c205-4558-b66b-f847062b8cf8\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"fc5bfc6c-c205-4558-b66b-f847062b8cf8\", :values ({:x 23.0, :y 0} {:x 25.363636363636363, :y 140.0} {:x 27.727272727272727, :y 30.0} {:x 30.09090909090909, :y 100.0} {:x 32.45454545454545, :y 30.0} {:x 34.81818181818182, :y 90.0} {:x 37.18181818181819, :y 60.0} {:x 39.545454545454554, :y 90.0} {:x 41.90909090909092, :y 20.0} {:x 44.27272727272729, :y 200.0} {:x 46.636363636363654, :y 110.0} {:x 49.00000000000002, :y 130.0} {:x 51.36363636363639, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}}}"}
-;; <=
 
 ;; **
 ;;; Visually it would appear that this occurs around 38-40 people.  We can algorithmically find the mode for potential subsequent use:
@@ -131,9 +122,6 @@
      (* bucket-size))
 
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-long'>43</span>","value":"43"}
-;; <=
 
 ;; @@
 
