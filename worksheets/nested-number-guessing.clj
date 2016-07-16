bc82d8deb283ee871ae8b3d6aa1b88932f753535
Frank Wood <fwood@robots.ox.ac.uk>
2016-07-16 08:40:19 -0400

    updated to new calling syntax with returns; made code more efficient, and clarified some explanation(s)

--- worksheets/nested-number-guessing.clj
+++ worksheets/nested-number-guessing.clj
@@ -22,68 +22,95 @@
 ;;; 	(hist (repeat 10000 sample))
 ;;; 
 ;;; 
-;;; Ours looks something like this:
+;;; Starting with our namespace boilerplate
 ;; **
 
 ;; @@
 (ns nested-number-guessing
-  (:require [gorilla-plot.core :as plot])
+  (:require [gorilla-plot.core :as plot]
+            [anglican.stat :as s])
   (:use clojure.repl
-        [anglican core runtime emit [state :only [get-predicts]]]))
+        [anglican core runtime emit [inference :only [collect-by]]
+         [state :only [get-predicts]]]))
 ;; @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
 ;; <=
 
 ;; **
-;;; We have to get rejection sampling semantics to impose equality.
+;;; Ours looks like this.  The first player has no prior knowledge so she picks uniformly and conditions on her constraint being satisfied, i.e. the two player sum being 13.  She knows that the second player will only choose numbers whose sum is greater than 8 but has no additional preference.
 ;; **
 
 ;; @@
-(defdist dirac
-  "Dirac distribution"
-  [x] []
-      (sample [this] x)
-      (observe [this value] (if (= x value) 0.0 (- (/ 1.0 0.0)))))
+(defm secondplayer [a] 
+  (let [c (- 8 a)]
+    (sample (uniform-discrete c 10))))
 
-(with-primitive-procedures [dirac] 
-  (defquery inner [a] 
-    (let [c (sample (uniform-discrete 0 10))]
-      (observe (dirac (> (+ a c) 8)) true)
-      (predict :c c))))
-
-;(:c (get-predicts (first 
-(with-primitive-procedures [doquery dirac get-predicts :log-weight]
-  (defquery outer []
-    (let [valid-sample (fn [x]  (= (- (/ 0.0 1.0)) (:log-weight x)))
-          a (sample (uniform-discrete 0 10))
-          b (:c (get-predicts (first (filter valid-sample (take 100 (doquery :smc inner [a] :debug true :number-of-particles 1))))))
-          ;_ (prn b)
-          ]
-      (observe (dirac (+ a b)) 13)
-      (predict :a a))))
-
+(defquery firstplayer []
+  (let [a (sample (uniform-discrete 0 10))
+        b (secondplayer a)]
+    (observe (dirac (+ a b)) 13)
+    a))
 ;; @@
 ;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;nested-number-guessing/outer</span>","value":"#'nested-number-guessing/outer"}
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;nested-number-guessing/firstplayer</span>","value":"#'nested-number-guessing/firstplayer"}
 ;; <=
 
 ;; **
-;;; The constant 100 in the take expression above is a horrible hack just to get it to work.
-;;; 
-;;; We can check that this generator produces a sample distribution that is in accordance with our understanding of the normal distribution.
+;;; When this query is run it suggests that she should never pick a number below 4 and that larger numbers are generally less desirable.
 ;; **
 
 ;; @@
-(plot/histogram (->>
-                  (doquery :lmh outer [])
-                  (filter #(= (- (/ 0.0 1.0)) (:log-weight %)))
-                  (take 10000)
-                  (map get-predicts)
-                  (map :a))
-                :normalise :probability)
+(->> (doquery :smc firstplayer [] :number-of-particles 100)
+     (take 50000)
+     (collect-by :result)
+     (s/empirical-distribution)
+     (merge (zipmap (range 0 10) (repeat 10 0)))
+     (into (sorted-map))
+     (#(plot/bar-chart (keys %) (vals %))))
 
 ;; @@
 ;; =>
-;;; {"type":"vega","content":{"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"fd9bab90-0371-4b71-aec7-1efcd2c7926c","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"fd9bab90-0371-4b71-aec7-1efcd2c7926c","field":"data.y"}}],"marks":[{"type":"line","from":{"data":"fd9bab90-0371-4b71-aec7-1efcd2c7926c"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"interpolate":{"value":"step-before"},"fill":{"value":"steelblue"},"fillOpacity":{"value":0.4},"stroke":{"value":"steelblue"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}}],"data":[{"name":"fd9bab90-0371-4b71-aec7-1efcd2c7926c","values":[{"x":4.0,"y":0},{"x":4.333333333333333,"y":0.2414},{"x":4.666666666666666,"y":0.0},{"x":4.999999999999999,"y":0.0},{"x":5.333333333333332,"y":0.2194},{"x":5.666666666666665,"y":0.0},{"x":5.999999999999998,"y":0.0},{"x":6.333333333333331,"y":0.1541},{"x":6.666666666666664,"y":0.0},{"x":6.999999999999997,"y":0.0},{"x":7.33333333333333,"y":0.1595},{"x":7.666666666666663,"y":0.0},{"x":7.9999999999999964,"y":0.0},{"x":8.33333333333333,"y":0.1175},{"x":8.666666666666664,"y":0.0},{"x":8.999999999999998,"y":0.0},{"x":9.333333333333332,"y":0.1081},{"x":9.666666666666666,"y":0}]}],"width":400,"height":247.2187957763672,"padding":{"bottom":40,"top":10,"right":10,"left":55}},"value":"#gorilla_repl.vega.VegaView{:content {:axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"fd9bab90-0371-4b71-aec7-1efcd2c7926c\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"fd9bab90-0371-4b71-aec7-1efcd2c7926c\", :field \"data.y\"}}], :marks [{:type \"line\", :from {:data \"fd9bab90-0371-4b71-aec7-1efcd2c7926c\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :interpolate {:value \"step-before\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 0.4}, :stroke {:value \"steelblue\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}}], :data [{:name \"fd9bab90-0371-4b71-aec7-1efcd2c7926c\", :values ({:x 4.0, :y 0} {:x 4.333333333333333, :y 0.2414} {:x 4.666666666666666, :y 0.0} {:x 4.999999999999999, :y 0.0} {:x 5.333333333333332, :y 0.2194} {:x 5.666666666666665, :y 0.0} {:x 5.999999999999998, :y 0.0} {:x 6.333333333333331, :y 0.1541} {:x 6.666666666666664, :y 0.0} {:x 6.999999999999997, :y 0.0} {:x 7.33333333333333, :y 0.1595} {:x 7.666666666666663, :y 0.0} {:x 7.9999999999999964, :y 0.0} {:x 8.33333333333333, :y 0.1175} {:x 8.666666666666664, :y 0.0} {:x 8.999999999999998, :y 0.0} {:x 9.333333333333332, :y 0.1081} {:x 9.666666666666666, :y 0})}], :width 400, :height 247.2188, :padding {:bottom 40, :top 10, :right 10, :left 55}}}"}
+;;; {"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":55,"bottom":40,"right":10},"data":[{"name":"41c0c627-14b3-49ef-8f4e-5f65407f9799","values":[{"x":0,"y":0},{"x":1,"y":0},{"x":2,"y":0},{"x":3,"y":0},{"x":4,"y":0.21639744952177695},{"x":5,"y":0.1975637619553692},{"x":6,"y":0.16327311370882236},{"x":7,"y":0.15036397449521907},{"x":8,"y":0.14085547290116968},{"x":9,"y":0.13154622741764288}]}],"marks":[{"type":"rect","from":{"data":"41c0c627-14b3-49ef-8f4e-5f65407f9799"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"41c0c627-14b3-49ef-8f4e-5f65407f9799","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"41c0c627-14b3-49ef-8f4e-5f65407f9799","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"41c0c627-14b3-49ef-8f4e-5f65407f9799\", :values ({:x 0, :y 0} {:x 1, :y 0} {:x 2, :y 0} {:x 3, :y 0} {:x 4, :y 0.21639744952177695} {:x 5, :y 0.1975637619553692} {:x 6, :y 0.16327311370882236} {:x 7, :y 0.15036397449521907} {:x 8, :y 0.14085547290116968} {:x 9, :y 0.13154622741764288})}], :marks [{:type \"rect\", :from {:data \"41c0c627-14b3-49ef-8f4e-5f65407f9799\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"41c0c627-14b3-49ef-8f4e-5f65407f9799\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"41c0c627-14b3-49ef-8f4e-5f65407f9799\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}
+;; <=
+
+;; **
+;;; It is interesting to note that Anglican' defm is not the same as a nested query.  defm allows code structuring and syntactic decomposition of the overall joint distribution.  In the above conditioning occurs by changing the lower bound of the second player's choice, exactly as desired.  
+;;; 
+;;; If one instead, wished to directly transcribe the code from ForestDB with the second player making nested query using a uniform draw from a uniform distribution over the (0 10] then in order to turn defm into the conditional distribution equivalent to a nested query one must weight the trace by the conditional distribution normalization constant.  
+;;; 
+;;; The following example is _not_ how one would write this query but instead serves to highlight some important semantic differences between nesting of queries (which we don't support in the same way as WebPPL) and code structuring decompositions of the joint.
+;;; 
+;;; To really get the difference consider what it means to have the (observe flip...) line in the following and see what happens if you comment it out.
+;; **
+
+;; @@
+(defm secondplayeralt [a] 
+  (let [c (sample (uniform-discrete 0 10))
+        Z (- 10 (- 8 a))]
+    (observe (dirac (> (+ a c) 8)) true)
+    (observe (flip (/ 1. Z)) true)
+    c))
+        
+(defquery firstplayeralt []
+  (let [a (sample (uniform-discrete 0 10))
+        b (secondplayeralt a)]
+    (observe (dirac (+ a b)) 13)
+    a))
+;; @@
+;; =>
+;;; {"type":"html","content":"<span class='clj-var'>#&#x27;nested-number-guessing/firstplayeralt</span>","value":"#'nested-number-guessing/firstplayeralt"}
+;; <=
+
+;; @@
+(->> (doquery :smc firstplayeralt [] :number-of-particles 100)
+     (take 50000)
+     (collect-by :result)
+     (s/empirical-distribution)
+     (merge (zipmap (range 0 10) (repeat 10 0)))
+     (into (sorted-map))
+     (#(plot/bar-chart (keys %) (vals %))))
+;; @@
+;; =>
+;;; {"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"top":10,"left":55,"bottom":40,"right":10},"data":[{"name":"f088694a-d9d4-48d7-aac2-d3fa1fb007eb","values":[{"x":0,"y":0.0},{"x":1,"y":0},{"x":2,"y":0.0},{"x":3,"y":0.0},{"x":4,"y":0.21373132422022853},{"x":5,"y":0.1929728577567388},{"x":6,"y":0.18311809738445908},{"x":7,"y":0.15492969342767676},{"x":8,"y":0.13571895437710707},{"x":9,"y":0.11952907283379022}]}],"marks":[{"type":"rect","from":{"data":"f088694a-d9d4-48d7-aac2-d3fa1fb007eb"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"width":{"scale":"x","band":true,"offset":-1},"y":{"scale":"y","field":"data.y"},"y2":{"scale":"y","value":0}},"update":{"fill":{"value":"steelblue"},"opacity":{"value":1}},"hover":{"fill":{"value":"#FF29D2"}}}}],"scales":[{"name":"x","type":"ordinal","range":"width","domain":{"data":"f088694a-d9d4-48d7-aac2-d3fa1fb007eb","field":"data.x"}},{"name":"y","range":"height","nice":true,"domain":{"data":"f088694a-d9d4-48d7-aac2-d3fa1fb007eb","field":"data.y"}}],"axes":[{"type":"x","scale":"x"},{"type":"y","scale":"y"}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:top 10, :left 55, :bottom 40, :right 10}, :data [{:name \"f088694a-d9d4-48d7-aac2-d3fa1fb007eb\", :values ({:x 0, :y 0.0} {:x 1, :y 0} {:x 2, :y 0.0} {:x 3, :y 0.0} {:x 4, :y 0.21373132422022853} {:x 5, :y 0.1929728577567388} {:x 6, :y 0.18311809738445908} {:x 7, :y 0.15492969342767676} {:x 8, :y 0.13571895437710707} {:x 9, :y 0.11952907283379022})}], :marks [{:type \"rect\", :from {:data \"f088694a-d9d4-48d7-aac2-d3fa1fb007eb\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :width {:scale \"x\", :band true, :offset -1}, :y {:scale \"y\", :field \"data.y\"}, :y2 {:scale \"y\", :value 0}}, :update {:fill {:value \"steelblue\"}, :opacity {:value 1}}, :hover {:fill {:value \"#FF29D2\"}}}}], :scales [{:name \"x\", :type \"ordinal\", :range \"width\", :domain {:data \"f088694a-d9d4-48d7-aac2-d3fa1fb007eb\", :field \"data.x\"}} {:name \"y\", :range \"height\", :nice true, :domain {:data \"f088694a-d9d4-48d7-aac2-d3fa1fb007eb\", :field \"data.y\"}}], :axes [{:type \"x\", :scale \"x\"} {:type \"y\", :scale \"y\"}]}}"}
 ;; <=
